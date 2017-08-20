33ebc5be317965adf1e05561f4e1a433664a98b0
Alexandr Zolotov <zol22222@mail.ru>
2017-08-20 23:54:53 +0700

    Add mobile version

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const gulp = require('gulp'),
 gulp.task('build', () => {
     gulp.src('app/index.html').pipe(gulp.dest('views'));
     gulp.src('app/styles.css').pipe(gulp.dest('views'));
+    gulp.src('app/stylesMobile.css').pipe(gulp.dest('views'));
 
      gulp.src('app/script.js')
          .pipe(babel({
